5731a6d81b2315b9a1b91c7bfffb0ae6972a585b
mmelonjareda <73959915+mmelonjareda@users.noreply.github.com>
2021-05-25 10:24:57 -0500

    Cambios script

--- Assets/Scripts/interactuar.cs
+++ Assets/Scripts/interactuar.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class interactuar : MonoBehaviour
 {
     public float incremento = 1.0f;
+
+    public float velocidad = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,6 @@ public class interactuar : MonoBehaviour
 
     private void OnMouseDown()
     {
-        transform.Translate(0, incremento*Time.deltaTime, 0);
+        transform.Translate(0, incremento*velocidad*Time.deltaTime, 0);
     }
 }
